afcae891e1b8bf6711b0e3aad3ae88afd17aa338
kadirerman <kadirermantr@gmail.com>
2023-04-02 04:17:25 +0300

    feat: set functions, routes and view files

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,23 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-
+        return view('chirps.index');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
 
+        $request->user()->chirps()->create($validated);
+
+        return redirect()->route('chirps.index');
     }
 }
