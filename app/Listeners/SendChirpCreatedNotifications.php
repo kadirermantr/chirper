45f1a53fdc553de00efe83d8b07248e11be8b6e1
kadirerman <kadirermantr@gmail.com>
2023-05-06 05:05:44 +0300

    refactor: edit code with pint

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -6,7 +6,6 @@ use App\Events\ChirpCreated;
 use App\Models\User;
 use App\Notifications\NewChirp;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 
 class SendChirpCreatedNotifications //implements ShouldQueue
 {
