fe37f8b349af84607aefbb336e77691c8ca7c8b4
kadirerman <kadirermantr@gmail.com>
2023-05-19 23:49:15 +0300

    refactor: update and group routes

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/', 'welcome');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store', 'edit', 'update', 'destroy'])
-    ->middleware(['auth', 'verified']);
+    Route::middleware('verified')->group(function () {
+        Route::view('/dashboard', 'dashboard')->name('dashboard');
+
+        Route::resource('chirps', ChirpController::class)
+            ->only(['index', 'store', 'edit', 'update', 'destroy']);
+    });
+});
 
 require __DIR__.'/auth.php';
